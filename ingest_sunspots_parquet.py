211ffcc385d3f2701844c51509580485b5646feb
Sharp <highonsharp@gmail.com>
2026-03-12 23:08:07 +0530

    added details

--- ingest_sunspots_parquet.py
+++ ingest_sunspots_parquet.py
@@ -5,12 +5,18 @@ import pandas as pd
 from datetime import datetime, timezone
 
 
-#Configuration
+#importing the data
 BASE = "https://services.swpc.noaa.gov/json/solar-cycle"
 FILENAME = "sunspots.json"
 URL = f"{BASE}/{FILENAME}"
+
+#if files are missing
 DATA_DIR = Path("data")
 RAW_DIR = DATA_DIR / "raw"
+
+#to store last fetch time
 METADATA_FILE = DATA_DIR / "metadata.json"
+
+#time-indexed datasheet
 PARQUET_FILE = DATA_DIR / "sunspots.parquet"
  
