3ee4bdb03c649f3d5b92e25ad596b9e4ea1736d9
Abhishek Menon <abhishek@Abhishek-Laptop.>
2026-03-15 02:45:15 +0000

    Data file download script uploaded, please put your curls commands here as well

--- update_data.sh
+++ update_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Download begins..."
 curl https://services.swpc.noaa.gov/json/planetary_k_index_1m.json -o data/planetary_k_index.json
-echo "One done"
+echo "planetary_k_index.json done"
 curl https://services.swpc.noaa.gov/json/boulder_k_index_1m.json -o data/boulder_k_index.json 
-echo "Two done"
+echo "boulder_k_index.json done" 
 curl https://services.swpc.noaa.gov/json/45-day-forecast.json -o data/45-day-forecast.json
 echo "All done"
