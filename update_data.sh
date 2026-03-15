2b05f9ebf625702b279ee35dc73b6f42d8439b89
Vansh Gupta <89036792+vanshgupta17@users.noreply.github.com>
2026-03-15 17:26:54 +0530

    Added solar flare dataset download
    Added curl command to fetch solar flares data.

--- update_data.sh
+++ update_data.sh
@@ -6,3 +6,6 @@ curl https://services.swpc.noaa.gov/json/boulder_k_index_1m.json -o data/boulder
 echo "boulder_k_index.json done" 
 curl https://services.swpc.noaa.gov/json/45-day-forecast.json -o data/45-day-forecast.json
 echo "All done"
+curl -L "https://services.swpc.noaa.gov/json/goes/primary/xray-flares-7-day.json" -o data/solar_flares.json
+echo "solar_flares.json done"
+echo "All done!"
